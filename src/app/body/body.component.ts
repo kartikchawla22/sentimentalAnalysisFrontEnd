eaf01c685445550bbe4c10a97aa7c494bfbdfefa
Kartik Chawla <kchawla1995@gmail.com>
2019-05-11 09:12:48 +0530

    final functionality

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ApicallService } from "../apicall.service";
+import * as jspdf from 'jspdf';
 
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
-lat;
-lng;
-query;
-  public doughnutChartLabels:string[] = ['Positive Tweets', 'Negative Tweets'];
-  public demodoughnutChartData:number[] = [10, 20];
-  public doughnutChartType:string = 'doughnut';
+  lat;
+  lng;
+  query;
+  showLoader: boolean = false;
+  public doughnutChartLabels: string[] = ['Positive Tweets', 'Negative Tweets', 'Neutral Tweets'];
+  public demodoughnutChartData: number[] = [];
+  public doughnutChartType: string = 'doughnut';
   public chartColors: Array<any> = [
     { // all colors in order
-      backgroundColor: ['#d13537', '#b0o0b5']
+      backgroundColor: ['#63FFCE', '#FF4863', '#A4A70D']
     }
   ];
-constructor(
+
+  constructor(
     private _apiservice: ApicallService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     if (navigator.geolocation) {
@@ -34,19 +39,42 @@ constructor(
   }
 
   onclick(el: string) {
-    const elem = document.querySelector(el);
-    let offsetTop = elem.getBoundingClientRect().top;
-    window.scroll({top: offsetTop, left: 0, behavior: 'smooth'} );
 
-// if(!!this.query) {
-//   this.demodoughnutChartData = [];
-//   this._apiservice.getGraph(this.lat, this.lng, this.query).subscribe((res) => {
-//     if (this.demodoughnutChartData.length === 0) {
-//       this.demodoughnutChartData.push(res.data.arr[0]);
-//       this.demodoughnutChartData.push(res.data.arr[1]);
-//     }
-//   })
-// }
+this.showLoader = true;
+    if (!!this.query) {
+      this.demodoughnutChartData = [];
+      this._apiservice.getGraph(this.lat, this.lng, this.query).subscribe((res) => {
+        if (this.demodoughnutChartData.length === 0) {
+          this.demodoughnutChartData.push(res.data.arr[0]);
+          this.demodoughnutChartData.push(res.data.arr[1]);
+          this.demodoughnutChartData.push(res.data.arr[2]);
+          setTimeout(() => {
+            const elem = document.querySelector(el);
+            if(!!elem) {
+              let offsetTop = elem.getBoundingClientRect().top;
+              window.scroll({top: offsetTop, left: 0, behavior: 'smooth'});
+            }
+            this.showLoader = false;
+          }, 100)
+        }
+      })
+    }
   }
+  public captureScreen()
+  {
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
 
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('Result.pdf'); // Generated PDF
+    });
+  }
 }
